bc8e3c2ca704f8a2cd14805ed12b92ec7eeb54fe
Lais Maruyama <laismaruyama@outlook.com>
2025-09-25 11:57:43 -0300

    ####

---
+++ cypress.config.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  env: {
+    "email": "clinica@gmail.com",
+    "senha": "4321",
+    "api_login": "https://vollmecypress-server-1.onrender.com/login",
+    "api_clinica": "https://vollmecypress-server-1.onrender.com/clinica",
+    "api_especialista": "https://vollmecypress-server-1.onrender.com/especialista",
+    "requestMode": true
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000/',
+    video: false,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss"
+    },
+    projectId: "8gi3z6",
+    
+    defaultCommandTimeout: 60000
+
+  },
+
+
+});
